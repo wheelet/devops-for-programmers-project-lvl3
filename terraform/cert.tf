6b0f2aa031eccbba12911af72b58334f500da491
Vadim Bondar <wheelet1228@gmail.com>
2021-08-04 03:35:18 +0300

    fix

--- terraform/cert.tf
+++
@@ -1,6 +0,0 @@
-resource "digitalocean_certificate" "cert" {
-  name = "cert"
-  type = "lets_encrypt"
-  domains = [
-    "terraform.wheelet.me"]
-}
--- terraform/ssh.tf
+++
@@ -1,3 +0,0 @@
-data "digitalocean_ssh_key" "ssh" {
-  name = "asd"
-}
