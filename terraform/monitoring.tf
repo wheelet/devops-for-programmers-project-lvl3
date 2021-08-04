b994def150f74e5d8994c702f549f4ebfd7d0686
Vadim Bondar <wheelet1228@gmail.com>
2021-08-04 05:32:59 +0300

    fix

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -4,4 +4,10 @@ resource "datadog_monitor" "healthcheck" {
   message = " @wheelet1228@gmail.com"
 
   query = "\"http.can_connect\".over(\"host:web-terraform-project-01\",\"host:web-terraform-project-02\").by(\"host\",\"instance\",\"url\").last(2).count_by_status()"
+
+  notify_no_data    = true
+  renotify_interval = 60
+
+  notify_audit = false
+  timeout_h    = 60
 }
