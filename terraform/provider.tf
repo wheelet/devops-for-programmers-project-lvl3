f1ac699c911d00dfd4b9e6ee92c2035b551e84fb
Vadim Bondar <wheelet1228@gmail.com>
2021-08-04 03:19:20 +0300

    fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,31 +5,17 @@ terraform {
       version = "~> 2.0"
     }
     datadog = {
-      source = "DataDog/datadog"
+      source  = "DataDog/datadog"
+      version = "~> 3.2.0"
     }
   }
 }
 
-variable "do_token" {
-  description = "DO Access Token"
-  type        = string
-}
-
-variable "datadog_api_key" {
-  type        = string
-  sensitive   = true
-}
-
-variable "datadog_app_key" {
-  type        = string
-  sensitive   = true
+provider "digitalocean" {
+  token = var.do_token
 }
 
 provider "datadog" {
   api_key = var.datadog_api_key
   app_key = var.datadog_app_key
-}
-
-provider "digitalocean" {
-  token = var.do_token
 }
