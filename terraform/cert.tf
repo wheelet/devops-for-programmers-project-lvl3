d11aa59b37e36335e60c5839672aa2e8fed7cdca
Vadim Bondar <wheelet1228@gmail.com>
2021-08-04 05:14:17 +0300

    fix

--- terraform/cert.tf
+++ terraform/cert.tf
@@ -1,5 +1,9 @@
 resource "digitalocean_certificate" "cert" {
-  name    = "cert"
+  name    = "asdcert"
   type    = "lets_encrypt"
   domains = ["terraform.wheelet.me"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
